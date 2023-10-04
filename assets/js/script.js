0accc5ba974fc57de1ec48ce9f2eb754b6d95746
Weronika Grotek <exo233@wp.pl>
2023-10-04 17:31:34 +0200

    Init

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    navigation:{
+        enabled: false
+    },
+    scrollbar: {
+        enabled: false
+    },
+    autoplay:{
+        delay: 5000
+    }
+});
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php ?>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <link rel="stylesheet" href="./assets/css/styles.css" />
+    <script  type="module" src="./assets/js/script.js"></script>
+    <title>Weronika slider</title>
+</head>
+
+<body>
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide"><img src="./static/img/kot1.jpg"></div>
+            <div class="swiper-slide"><img src="./static/img/kot2.jpg"></div>
+            <div class="swiper-slide"><img src="./static/img/kot3.jpg"></div>
+        </div>
+        <div class="swiper-content">
+            <h1>To zdjęcia kotów</h1>
+        </div>
+    </div>
+</body>
+
+</html>
